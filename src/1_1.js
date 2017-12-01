446d54d002fd0edd762f09278e1a96b1e9f42aea
Jan Pfeiffer <liberry@ymail.com>
2017-12-01 16:25:21 +0100

    amend

---
+++ src/1_1.js
@@ -0,0 +1,16 @@
+import fs from 'fs'
+import path from 'path'
+
+const input = fs.readFileSync(path.resolve(__dirname, '1_1.txt'), 'utf8')
+
+let sum = 0
+
+for (let i = 0; i < input.length - 1; i++) {
+  if (i === input.length - 2) {
+    sum += (input[i] === input[0]) ? +input[i] : 0
+  } else {
+    sum += (input[i] === input[i+1]) ? +input[i] : 0
+  }
+}
+
+console.log(sum)
