efbffaaa12df062f135ab9e775b2ef5d87928911
Jan Pfeiffer <liberry@ymail.com>
2017-12-01 23:34:37 +0100

    refactored 1_1 a bit, completed 1_2

--- src/1_1.js
+++ src/1_1.js
@@ -1,16 +1,11 @@
 import fs from 'fs'
 import path from 'path'
 
-const input = fs.readFileSync(path.resolve(__dirname, '1_1.txt'), 'utf8')
+const input = fs.readFileSync(path.resolve(__dirname, '1_1.txt'), 'utf8').trim()
 
 let sum = 0
 
-for (let i = 0; i < input.length - 1; i++) {
-  if (i === input.length - 2) {
-    sum += (input[i] === input[0]) ? +input[i] : 0
-  } else {
-    sum += (input[i] === input[i+1]) ? +input[i] : 0
-  }
+for (let i = 0; i < input.length; i++) {
+  sum += (input[i] === input[(i+1) % input.length]) ? +input[i] : 0
 }
-
-console.log(sum)
+console.log(`Answer for Day 1, Puzzle 1:\n${sum}`)
--- src/1_2.js
+++ src/1_2.js
@@ -1,1 +1,13 @@
-console.log('working')
+import fs from 'fs'
+import path from 'path'
+
+const input = fs.readFileSync(path.resolve(__dirname, '1_1.txt'), 'utf8').trim()
+
+let sum = 0
+
+const length = input.length
+for (let i = 0; i < length; i++) {
+  sum += (input[i] === input[(i + (length/2)) % length]) ? +input[i] : 0
+}
+
+console.log(`Answer for Day 1, Puzzle 2:\n${sum}`)
