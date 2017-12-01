a0932a6674ec06149b820dedaf6cbaad48262b77
Jan Pfeiffer <liberry@ymail.com>
2017-12-01 23:42:18 +0100

    refactoring

--- src/1_2.js
+++ src/1_2.js
@@ -5,9 +5,14 @@ const input = fs.readFileSync(path.resolve(__dirname, '1_1.txt'), 'utf8').trim()
 
 let sum = 0
 
-const length = input.length
-for (let i = 0; i < length; i++) {
-  sum += (input[i] === input[(i + (length/2)) % length]) ? +input[i] : 0
+for (let i = 0, length = input.length; i < length; i++) {
+  const curr = +input[i]
+  const nextIndex = (i + (length / 2)) % length
+  const next = +input[nextIndex]
+
+  if (curr === next) {
+    sum += curr
+  }
 }
 
 console.log(`Answer for Day 1, Puzzle 2:\n${sum}`)
